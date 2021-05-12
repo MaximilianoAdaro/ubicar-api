48b5696e7ebd4a918e38c4cfbf41e5093a33baa1
katiacammisa <katia.cammisa@ing.austral.edu.ar>
2021-05-12 20:44:33 -0300

    Pagination of listing

--- src/main/kotlin/com/ubicar/ubicar/controllers/PropertyController.kt
+++ src/main/kotlin/com/ubicar/ubicar/controllers/PropertyController.kt
@@ -6,10 +6,10 @@ import com.ubicar.ubicar.dtos.PropertyPreviewDTO
 import com.ubicar.ubicar.factories.PropertyFactory
 import com.ubicar.ubicar.factories.PropertyPreviewFactory
 import com.ubicar.ubicar.services.property.PropertyService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.*
 
 @Controller
 class PropertyController(private val propertyService: PropertyService) {
@@ -18,8 +18,8 @@ class PropertyController(private val propertyService: PropertyService) {
     private val propertyPreviewFactory = PropertyPreviewFactory()
 
     @GetMapping("/preview")
-    fun getProperties(): List<PropertyPreviewDTO> {
-        return propertyService.findAll().map { propertyPreviewFactory.convert(it) }
+    fun getProperties(@RequestParam page: Int): Page<PropertyPreviewDTO> {
+        return propertyService.findAll(PageRequest.of(page, 16)).map { propertyPreviewFactory.convert(it) }
     }
 
     @PostMapping("/create")
--- src/main/kotlin/com/ubicar/ubicar/repositories/PropertyRepository.kt
+++ src/main/kotlin/com/ubicar/ubicar/repositories/PropertyRepository.kt
@@ -1,11 +1,13 @@
 package com.ubicar.ubicar.repositories
 
 import com.ubicar.ubicar.entities.Property
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface PropertyRepository : CrudRepository<Property, Long> {
 
-    @Query(value = "select p from Property p")
-    override fun findAll() : List<Property>
+//    @Query(value = "select p from Property p")
+    fun findAll(pageable: Pageable) : Page<Property>
 }
