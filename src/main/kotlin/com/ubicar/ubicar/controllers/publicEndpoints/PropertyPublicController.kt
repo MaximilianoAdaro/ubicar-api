dd93b25bd2c901713d5e2d985bf6a70f2b5cf7e5
katiacammisa <katia.cammisa@ing.austral.edu.ar>
2021-08-17 18:33:17 -0300

    Save user filters

--- src/main/kotlin/com/ubicar/ubicar/controllers/publicEndpoints/PropertyPublicController.kt
+++ src/main/kotlin/com/ubicar/ubicar/controllers/publicEndpoints/PropertyPublicController.kt
@@ -4,12 +4,16 @@ import com.ubicar.ubicar.dtos.*
 import com.ubicar.ubicar.dtos.filter.PropertyFilterDto
 import com.ubicar.ubicar.dtos.filter.PropertyLazyTableDto
 import com.ubicar.ubicar.dtos.filter.PropertySort
+import com.ubicar.ubicar.factories.filter.FilterFactory
 import com.ubicar.ubicar.factories.property.PropertyFactory
 import com.ubicar.ubicar.factories.property.PropertyPreviewFactory
+import com.ubicar.ubicar.services.filter.FilterService
 import com.ubicar.ubicar.services.property.PropertyService
+import com.ubicar.ubicar.services.user.UserService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -24,7 +28,9 @@ import java.util.Optional
 class PropertyPublicController(
   private val propertyService: PropertyService,
   private val propertyFactory: PropertyFactory,
-  private val propertyPreviewFactory: PropertyPreviewFactory
+  private val propertyPreviewFactory: PropertyPreviewFactory,
+  private val filterService: FilterService,
+  private val filterFactory: FilterFactory
 ) {
 
   @GetMapping("/preview")
@@ -40,6 +46,7 @@ class PropertyPublicController(
     @RequestParam(value = "direction", required = false) direction: Optional<Sort.Direction>,
     @RequestParam(value = "property", required = false) property: Optional<PropertySort>,
   ): Page<PropertyPreviewDTO> {
+    if (SecurityContextHolder.getContext().authentication != null) filterService.save(filterFactory.from(filter))
     val propertyLazyTableDto = PropertyLazyTableDto(
       page.orElse(0),
       size.orElse(16),
