36764fedad6a2be8c8c836ad9462a3f68ddab6fa
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-08-22 20:16:51 -0300

    null style check

--- src/main/kotlin/com/ubicar/ubicar/factories/filter/FilterFactory.kt
+++ src/main/kotlin/com/ubicar/ubicar/factories/filter/FilterFactory.kt
@@ -2,16 +2,16 @@ package com.ubicar.ubicar.factories.filter
 
 import com.ubicar.ubicar.dtos.filter.PropertyFilterDto
 import com.ubicar.ubicar.entities.Filter
-import com.ubicar.ubicar.entities.User
-import com.ubicar.ubicar.entities.UserOrigin
 import com.ubicar.ubicar.factories.AbstractFactory
 import com.ubicar.ubicar.repositories.property.StyleRepository
 import com.ubicar.ubicar.services.user.UserService
 import org.springframework.stereotype.Component
 
 @Component
-class FilterFactory(private val styleRepository: StyleRepository,
-                    private val userService: UserService) : AbstractFactory<Filter, PropertyFilterDto> {
+class FilterFactory(
+  private val styleRepository: StyleRepository,
+  private val userService: UserService
+) : AbstractFactory<Filter, PropertyFilterDto> {
 
   override fun convert(input: Filter): PropertyFilterDto {
     return PropertyFilterDto(
@@ -31,10 +31,11 @@ class FilterFactory(private val styleRepository: StyleRepository,
   }
 
   fun from(filterDto: PropertyFilterDto): Filter {
+    val style = if (filterDto.style.isNullOrBlank()) null else styleRepository.findFirstByLabel(filterDto.style!!).orElseThrow()
     return Filter(
       filterDto.condition,
       filterDto.typeProperty,
-      styleRepository.findFirstByLabel(filterDto.style!!).get(),
+      style,
       filterDto.minPrice,
       filterDto.maxPrice,
       filterDto.minAmountRoom,
