114c4fd29f29ebe13f83b0b7a7c4e8aab39fa87c
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-09-26 16:37:53 -0300

    create csv when saving a property in last step - with all properties needed

--- src/main/kotlin/com/ubicar/ubicar/services/property/CsvPropertyServiceImpl.kt
+++ src/main/kotlin/com/ubicar/ubicar/services/property/CsvPropertyServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.ubicar.ubicar.services.property
 
 import com.ubicar.ubicar.entities.Property
+import com.ubicar.ubicar.entities.TypeOfProperty
 import com.ubicar.ubicar.repositories.property.PropertyRepository
 import com.ubicar.ubicar.services.geoSpatialService.GeoSpatialService
 import javassist.NotFoundException
@@ -15,6 +16,9 @@ class CsvPropertyServiceImpl(
   private val geoSpatialService: GeoSpatialService
 ) : CsvPropertyService {
 
+  val propertyDefaultColumnsList =
+    listOf("price_aprox_usd", "surface_total_in_m2", "surface_covered_in_m2", "price_usd_per_m2", "rooms")
+
   var spatialColumnsList = listOf(
     "dRailway",
     "dIndustrialArea",
@@ -28,6 +32,9 @@ class CsvPropertyServiceImpl(
     "dTrainStation",
     "dUniversity"
   )
+
+  var propertyTypeColumnsList =
+    listOf("property_type_PH", "property_type_apartment", "property_type_house", "property_type_store")
 
   override fun createCsvFromProperty(property: Property) {
     try {
@@ -52,8 +59,10 @@ class CsvPropertyServiceImpl(
   private fun loadColumns(): String {
 
     // list of columns for the CSV
-    val list = mutableListOf("id", "title", "description")
+    val list = mutableListOf<String>()
+    list.addAll(propertyDefaultColumnsList)
     list.addAll(spatialColumnsList)
+    list.addAll(propertyTypeColumnsList)
 
     val joiner = StringJoiner(",")
     list.forEach { joiner.add(it) }
@@ -61,14 +70,58 @@ class CsvPropertyServiceImpl(
   }
 
   private fun propetyData(property: Property): String {
+    val propertyDefaultResult = this.propertyDefaultResult(property)
     val runGeoDataUpdate = geoSpatialService.runGeoDataUpdate(property.address!!.coordinates).map { it.toString() }
+    val propertyTypeResult = this.propertyTypeResult(property).map { it.toString() }
 
     // list of the property data columns for the CSV
-    val list = mutableListOf(property.id, property.title, property.parkDescription)
-
+    val list = mutableListOf<String>()
+    list.addAll(propertyDefaultResult)
     list.addAll(runGeoDataUpdate)
+    list.addAll(propertyTypeResult)
+
     val joiner = StringJoiner(",")
     list.forEach { joiner.add(it) }
     return joiner.toString()
+  }
+
+  private fun propertyTypeResult(property: Property): List<Int> {
+    val type_PH = listOf(1, 0, 0, 0)
+    val type_APARTMENT = listOf(0, 1, 0, 0)
+    val type_HOUSE = listOf(0, 0, 1, 0)
+    val type_STORE = listOf(0, 0, 0, 1)
+
+    return when (property.type) {
+      TypeOfProperty.Casa -> type_HOUSE
+      TypeOfProperty.Quinta -> type_HOUSE
+      TypeOfProperty.Departamento -> type_APARTMENT
+      TypeOfProperty.Hotel -> type_APARTMENT
+      TypeOfProperty.Compartido -> type_HOUSE
+      TypeOfProperty.PH -> type_PH
+      TypeOfProperty.Local -> type_STORE
+      TypeOfProperty.Edificio -> type_APARTMENT
+      TypeOfProperty.Loft -> type_APARTMENT
+      TypeOfProperty.Oficina -> type_APARTMENT
+      TypeOfProperty.Consultorio -> type_STORE
+      TypeOfProperty.Country -> type_HOUSE
+      TypeOfProperty.Flat -> type_APARTMENT
+      TypeOfProperty.Galpon -> type_STORE
+      TypeOfProperty.Terreno -> type_HOUSE
+      TypeOfProperty.Cochera -> type_HOUSE
+    }
+  }
+
+  // "price_aprox_usd", "surface_total_in_m2", "surface_covered_in_m2", "price_usd_per_m2", "rooms"
+  private fun propertyDefaultResult(property: Property): List<String> {
+    val price = property.price
+    val squareFoot = property.squareFoot
+    val price_usd_per_m2 = squareFoot?.let { price.div(it) }
+    return listOf(
+      price.toString(),
+      squareFoot.toString(),
+      property.coveredSquareFoot.toString(),
+      price_usd_per_m2.toString(),
+      property.rooms.toString()
+    )
   }
 }
